4f2fb972ffebcbb1b7d6d71821018c4f775527b1
nbitsmeanstack49 <nbitsmeanstack49@gmail.com>
2018-10-03 09:10:22 +0530

    property binding event binding

--- angular/basic/src/app/app.component.ts
+++ angular/basic/src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Ravi';
+  srcPath = './favicon.ico';
+  color = 'orange';
+  width = '500px';
+  information='';
+  toggle(){
+    this.color = this.color === 'orange' ? 'lightgrey' : 'orange'
+  }
+  changeColor(val){
+    console.log(val)
+    this.color = val;
+  }
+  update(val){
+    console.log(val);
+    this.information = val;
+  }
+  
 }
